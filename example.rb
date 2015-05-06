fb5c346eeb8f7b27f8191628f9f3cf07f9dfe498
Mando Escamilla <mando@mando.org>
2015-05-06 10:38:26 -0500

    Add #reducible?

--- example.rb
+++ example.rb
@@ -1,16 +1,11 @@
 puts <<-EX
-Add.new(
-  Multiply.new(Number.new(1), Number.new(2)),
-  Multiply.new(Number.new(3), Number.new(4))
-)
+
+Number.new(1).reducible?
+
 EX
 
 puts <<-EX
-Multiply.new(
-  Number.new(1),
-  Multiply.new(
-    Add.new(Number.new(2), Number.new(3)),
-    Number.new(4)
-  )
-)
+
+Add.new(Number.new(1), Number.new(2)).reducible?
+
 EX
