39092b2f23f8fd03af29511a2ca910a1962a829e
Mando Escamilla <mando@mando.org>
2015-05-06 11:58:03 -0500

    If implementation

--- example.rb
+++ example.rb
@@ -1,7 +1,25 @@
 puts <<-EX
 
 Machine.new(
-  Assign.new(:x, Add.new(Variable.new(:x), Number.new(1))),
-  { :x => Number.new(2) }
+  If.new(
+    Variable.new(:x),
+    Assign.new(:y, Number.new(1)),
+    Assign.new(:y, Number.new(2))
+  ),
+  { :x => Boolean.new(true) }
 ).run
+
+EX
+
+puts <<-EX
+
+Machine.new(
+  If.new(
+    Variable.new(:x),
+    Assign.new(:y, Number.new(1)),
+    DoNothing.new
+  ),
+  { :x => Boolean.new(false) }
+).run
+
 EX
