1bd8b3dce9f2fa1d8469025e3318d30c00ca4ffc
Mando Escamilla <mando@mando.org>
2015-05-06 10:32:05 -0500

    Cleanup empty implementation output

--- example.rb
+++ example.rb
@@ -4,3 +4,13 @@ Add.new(
   Multiply.new(Number.new(3), Number.new(4))
 )
 EX
+
+puts <<-EX
+Multiply.new(
+  Number.new(1),
+  Multiply.new(
+    Add.new(Number.new(2), Number.new(3)),
+    Number.new(4)
+  )
+)
+EX
