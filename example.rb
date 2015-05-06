396f88560a1858423c42cd57c23c33bc12a49322
Mando Escamilla <mando@mando.org>
2015-05-06 12:11:36 -0500

    While implementation

--- example.rb
+++ example.rb
@@ -1,11 +1,11 @@
 puts <<-EX
 
 Machine.new(
-  Sequence.new(
-    Assign.new(:x, Add.new(Number.new(1), Number.new(1))),
-    Assign.new(:y, Add.new(Variable.new(:x), Number.new(3)))
+  While.new(
+    LessThan.new(Variable.new(:x), Number.new(5)),
+    Assign.new(:x, Multiply.new(Variable.new(:x), Number.new(3)))
   ),
-  { }
+  { :x => Number.new(1) }
 ).run
 
 EX
