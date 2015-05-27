edb625b56b1385df0f5f007ed0a46d8cee621364
Matt Bridges <mbridges.91@gmail.com>
2015-05-27 13:33:09 -0500

    {Boolean => Number}

--- example.rb
+++ example.rb
@@ -2,7 +2,7 @@ puts <<-EX
 
 Machine.new(
   Sequence.new(
-    Assign.new(:x, Boolean.new(true)),
+    Assign.new(:x, Number.new(3)),
     Assign.new(:x, Add.new(Variable.new(:x), Number.new(1)))
   ),
   { }
